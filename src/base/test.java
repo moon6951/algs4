b4e283ab918228634c225866d8d84a037b43b00d
tianxin <m13389136951@163.com>
2022-06-12 17:08:20 +0800

    单语句代码段不需要花括号

--- src/base/test.java
+++
@@ -1,9 +0,0 @@
-package base;
-
-import edu.princeton.cs.algs4.BinaryIn;
-
-public class test {
-    public static void main(String[] args) {
-        BinaryIn.main(new String[]{"123"});
-    }
-}
---
+++ src/grammer/SingleStatementCode.java
@@ -0,0 +1,8 @@
+package grammer;
+
+public class SingleStatementCode {
+    public static void main(String[] args) {
+        if (2>1) System.out.println("123");
+        while (2>1) System.out.println("321");
+    }
+}
